1572ac12104a03421542877f2366dec64182a1c5
Your Name <you@example.com>
2025-10-03 10:11:48 +0530

    db connect

--- food-blog-app/backend/config/connectionDB.js
+++ food-blog-app/backend/config/connectionDB.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 
 const connectDB=async()=>{
-    await mongoose.connect(process.env.CONNECTION_STRING)
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log("connected..."))
 }
 
